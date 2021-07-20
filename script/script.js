b02b47f1db769e60a0bd38e3d488a0bcf5a4bc4a
Kenneth Walker <kennethesteban@hotmail.com>
2021-07-19 19:15:53 -0600

    Final Commit

---
+++ script/script.js
@@ -0,0 +1,6 @@
+function generateCat(){
+    let image = document.createElement('img');
+    let div = document.getElementById('flex-box-div');
+    image.src = "https://media1.tenor.com/images/bead0ffe77a3fd513511c05dafb492e6/tenor.gif?itemid=18105609";
+    div.appendChild(image);
+}
